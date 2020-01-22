e2dcd0a161bc5f454484ccc3611fa7aaff25377d
elaria <elarya.latif@gmail.com>
2020-01-22 15:53:49 +0200

    slide show images

--- admin/dist/js/ui.js
+++ admin/dist/js/ui.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
     $("#g_images #addSlideModal .addSlide").click(function() {
         var description = $("#descriptionSlide").val();
         var x = $('td.number').html(function(i, val) { return +val + 1 });
-        console.log(x);
         var image = $("#g_images #addSlideModal .image_show img").attr('src');
-        var markup = "<tr><td class='number'>1</td><td class='image_show'><img src= " + image + "  /></td><td>" + description + "</td><td class='edit_image'><input type='file' name='file' value='Upload Image' /><span class='label label-primary' ><i class = 'fa fa-edit'></i></span> </td><td class='delete_image'><span class='label label-danger'><i class='fa fa-trash'></i> </span></td> </tr>";
+        var markup = "<tr><td class='number'>1</td><td class='image_show'><img src= " + image + "  /></td><td class='slideDesc'>" + description + "</td><td class='edit_image'><input type='button' name='editSlideModal' value='Upload Image' data-toggle='modal' data-target='#editSlideModal' /><span class='label label-primary' ><i class = 'fa fa-edit'></i></span> </td><td class='delete_image'><span class='label label-danger'><i class='fa fa-trash'></i> </span></td> </tr>";
         $("#g_images tbody").append(markup);
     });
     // Add slide
@@ -23,7 +22,7 @@ $(document).ready(function() {
         }
     });
     // delete image row
-    $("#g_images .delete_image").click(function() {
-        $(this).parents("tr").remove();
+    $("#g_images").on("click", ".delete_image", function() {
+        $(this).closest("tr").remove();
     });
 });
